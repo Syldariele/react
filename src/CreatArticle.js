86fc41093934038ccf00c70883935bf95345a64a
Sitael <beru666@live.fr>
2020-04-02 14:40:29 +0200

    add react-bootstrap

--- src/CreatArticle.js
+++ src/CreatArticle.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
 
 const CreateArticle = () => {
     const [ title, setTitle ] = useState("");
@@ -37,30 +40,38 @@ const CreateArticle = () => {
     }
 
     return(
-        <form onSubmit={handleSubmit}>
-            <input
-                type="text"
-                name="title"
-                onChange={handleChange}
-                value={title}
-                placeholder="titre de l'article"
-            />
-            <textarea
-                name="content"
-                onChange={handleChange}
-                value={content}
-                placeholder="contenu de l'article"
-             >
-            </textarea>
-            <input
-                type="number"
-                name="author"
-                onChange={handleChange}
-                value={author}
-                placeholder="id de l'auteur"
-             />
-             <button type="submit">Valider</button>
-        </form>
+        <Container>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="article.title">
+                    <Form.Label>Titre de l'article</Form.Label>
+                    <Form.Control
+                        type="text"
+                        name="title"
+                        onChange={handleChange}
+                        value={title}
+                    />
+                </Form.Group>
+                <Form.Group controlId="article.content">
+                    <Form.Label>Contenu de l'article</Form.Label>
+                    <Form.Control
+                        as="textarea"
+                        name="content"
+                        onChange={handleChange}
+                        value={content}
+                    />
+                </Form.Group>
+                <Form.Group controlId="article.author">
+                    <Form.Label>Id de l'auteur</Form.Label>
+                    <Form.Control
+                        type="number"
+                        name="author"
+                        onChange={handleChange}
+                        value={author}
+                     />
+                </Form.Group>
+                 <Button  variant="primary" type="submit">Valider</Button>
+            </Form>
+        </Container>
     )
 };
 
