8e04641be93df277c9f49bc1f518a722aabbb136
Sitael <beru666@live.fr>
2020-04-02 16:21:49 +0200

    add react-router-dom, create home and Not found pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
-import DeleteArticle from "./DeleteArticle";
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+} from 'react-router-dom';
+
+import CreateArticle from "./pages/CreatArticle";
+import DeleteArticle from "./pages/DeleteArticle";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+
 const App = () => {
   return (
-    <div>
-      <DeleteArticle/>
-    </div>
+    <Router>
+      <Switch>
+          <Route exact path="/"        component={Home}/>
+        <Route path="/articles/create" component={CreateArticle}/>
+        <Route path="/articles/delete" component={DeleteArticle}/>
+        <Route path="*"                component={NotFound}/>
+      </Switch>
+    </Router>
   );
 }
 
