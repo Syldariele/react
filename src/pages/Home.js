5f06b73c5f689a5f83c458f68bc6c42270ec3fcc
Sitael <beru666@live.fr>
2020-04-06 16:57:23 +0200

    add fetch on Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 
 const Home = () => {
+    const [ articles, setArticles ] = useState([]);
+
+    useEffect(() => {
+        fetch( 'http://localhost:3001/api/articles')
+            .then((result) => {
+                return result.json();
+            })
+            .then(({ status, articles }) => {
+                if (status === "OK") {
+                    setArticles(articles);
+                } else {
+                    console.log("error : ", status);
+                }
+            })
+            .catch((error) => {
+                console.log("error : ", error);
+            });
+    }, []);
     return (
         <div>
             <h1>Page d'accueil</h1>
+            <h2>Derniers articles</h2>
         </div>
     );
 };
