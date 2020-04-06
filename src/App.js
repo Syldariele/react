62aa58d8fd1022ab1611778cbcb1d663c3aa784f
Sitael <beru666@live.fr>
2020-04-06 16:08:16 +0200

    add CreateComments.js & DeleteComments.js

--- src/App.js
+++ src/App.js
@@ -11,16 +11,20 @@ import CreateArticle from "./pages/CreatArticle";
 import DeleteArticle from "./pages/DeleteArticle";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
+import CreateComment from "./pages/CreateComment";
+import DeleteComment from "./pages/DeleteComment";
 
 const App = () => {
   return (
     <Router>
       <Navigation/>
       <Switch>
-          <Route exact path="/"        component={Home}/>
-        <Route path="/articles/create" component={CreateArticle}/>
-        <Route path="/articles/delete" component={DeleteArticle}/>
-        <Route path="*"                component={NotFound}/>
+          <Route exact path="/"          component={Home}/>
+        <Route path="/articles/create"   component={CreateArticle}/>
+        <Route path="/articles/delete"   component={DeleteArticle}/>
+          <Route path="/comments/create" component={CreateComment}/>
+          <Route path="/comments/delete" component={DeleteComment}/>
+        <Route path="*"                  component={NotFound}/>
       </Switch>
     </Router>
   );
---
+++ src/pages/CreateComment.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+
+const CreateComment = () => {
+    const [content, setContent] = useState("");
+    const [author, setAuthor] = useState("");
+    const [idArticle, setIdArticle] = useState("");
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("Content :", content);
+        console.log("Author : ", author);
+        console.log("Id Article : ", idArticle);
+    }
+
+    const handleChange = (event) => {
+        console.log("Target Name :", event.target.name);
+        console.log("Target Value :", event.target.value);
+
+        switch (event.target.name) {
+
+            case "content":
+                setContent(event.target.value);
+                break;
+            case "author":
+                setAuthor(event.target.value);
+                break;
+            case "idArticle":
+                setIdArticle(event.target.value);
+                break;
+            // no default
+        }
+    }
+
+    return (
+        <Container>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group controlId="comment.content">
+                    <Form.Label>Contenu du commentaire</Form.Label>
+                    <Form.Control
+                        as="textarea"
+                        name="content"
+                        onChange={handleChange}
+                        value={content}
+                    />
+                </Form.Group>
+                <Form.Group controlId="comment.author">
+                    <Form.Label>ID de l'auteur</Form.Label>
+                    <Form.Control
+                        type="number"
+                        name="author"
+                        onChange={handleChange}
+                        value={author}
+                     />
+                </Form.Group>
+                <Form.Group controlId="comment.idArticle">
+                    <Form.Label>Id de l'article</Form.Label>
+                    <Form.Control
+                        type="number"
+                        name="idArticle"
+                        onChange={handleChange}
+                        value={idArticle}
+                    />
+                </Form.Group>
+                <Button variant="primary" type="submit">Créer un commentaire</Button>
+            </Form>
+        </Container>
+    );
+};
+
+export default CreateComment;
