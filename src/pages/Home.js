6539a9b12a7a726e06654edce8dc92ad0aabb857
Sitael <beru666@live.fr>
2020-04-07 17:00:09 +0200

    add react-bootstrap/Card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,10 @@
 import React, { useState, useEffect} from 'react';
 
+import Card from 'react-bootstrap/Card';
+import CardGroup from 'react-bootstrap/CardGroup';
+import Container from 'react-bootstrap/Container';
+
+
 const Home = () => {
     const [ articles, setArticles ] = useState([]);
 
@@ -19,11 +24,40 @@ const Home = () => {
                 console.log("error : ", error);
             });
     }, []);
+
+    const renderedArticles = articles.map((article) => {
+        const date = new Date(article.created_at);
+        return (
+            <Card>
+                <Card.Header>
+                    <Card.Title as="h5">
+                        {article.title}
+                    </Card.Title>
+                </Card.Header>
+                    <Card.Body>
+                        <Card.Text>
+                            {article.content}
+                        </Card.Text>
+                    </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">
+                        crée le&nbsp;
+                        {date.getDate()}/{date.getMonth()+1}/{date.getFullYear()}&nbsp;
+                        par {article.authorFirstname} {article.authorLastname.substring(0, 1)}.
+                    </small>
+                </Card.Footer>
+            </Card>
+        );
+    });
+
     return (
-        <div>
+        <Container>
             <h1>Page d'accueil</h1>
             <h2>Derniers articles</h2>
-        </div>
+            <CardGroup>
+            {renderedArticles}
+            </CardGroup>
+        </Container>
     );
 };
 
