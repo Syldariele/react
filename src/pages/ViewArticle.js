6856508a83bce15559c388650565e58186d974da
Sitael <beru666@live.fr>
2020-04-09 09:55:19 +0200

    correction de articles en article

--- src/pages/ViewArticle.js
+++ src/pages/ViewArticle.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 
 import { formatDate} from "../utils/date";
+import { toast } from "react-toastify";
 
 import Container from "react-bootstrap/Container";
 
@@ -13,12 +14,20 @@ const ViewArticle = ({ match }) => {
     useEffect(() => {
          fetch('http://localhost:3001/api/article?id='+ id)
              .then((result) => {
-                 console.log(result.status);
+                 return result.json();
+             })
+             .then(({ status, article }) => {
+                 if (status === "OK") {
+                     setArticle(article);
+                 } else {
+                     toast.error("Oups... Une erreur est survenue !");
+                 }
              })
              .catch((error) => {
+                 toast.error("Oups... Une erreur est survenue !");
                  console.log(error);
              })
-    }, []);
+    }, [ id ]);
 
     return(
         <Container>
@@ -28,7 +37,7 @@ const ViewArticle = ({ match }) => {
             </p>
             <p>
                 posté le {formatDate(new Date())}<br/>
-                par {article.authoFirstname} {article.authorLastname}
+                par {article.authorFirstname} {article.authorLastname}
             </p>
             <div>
                 Commentaires
