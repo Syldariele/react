04709986b391aa637c39f4f07f37bca261cb227f
Sitael <beru666@live.fr>
2020-04-09 14:12:07 +0200

    add fetch of api/comments and renderedComments on ViewArticle.js

--- src/pages/ViewArticle.js
+++ src/pages/ViewArticle.js
@@ -4,12 +4,14 @@ import { formatDate} from "../utils/date";
 import { toast } from "react-toastify";
 
 import Container from "react-bootstrap/Container";
+import Card from "react-bootstrap/Card";
 
 const ViewArticle = ({ match }) => {
     const { id } = match.params;
     console.log(id);
 
     const [ article, setArticle ] = useState({});
+    const [ comments, setComments] = useState([]);
 
     useEffect(() => {
          fetch('http://localhost:3001/api/article?id='+ id)
@@ -29,6 +31,44 @@ const ViewArticle = ({ match }) => {
              })
     }, [ id ]);
 
+    useEffect(() => {
+        fetch('http://localhost:3001/api/comments?id=' + id)
+            .then((result) => {
+                return result.json();
+            })
+            .then(({ status, comments}) => {
+                if (status === "OK") {
+                    setComments(comments);
+                } else {
+                    toast.error("Oups... Une erreur est survenue !");
+                }
+            })
+            .catch((error) => {
+                toast.error("Oups... Une erreur est survenue !");
+                console.log(error);
+            })
+    }, [ id ]);
+
+    const renderedComments = comments.map((comment) => {
+        const { content, created_at, authorFirstname, authorLastname } = comment;
+        return (
+            <Card>
+                <Card.Body>
+                    <Card.Text>
+                        {content}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small className="text-muted">
+                        crée le&nbsp;
+                        { formatDate(created_at)}&nbsp;
+                        par {authorFirstname}&nbsp;{authorLastname.substring(0, 1)}.
+                    </small>
+                </Card.Footer>
+            </Card>
+        );
+    });
+
     return(
         <Container>
             <h1>{article.title}</h1>
@@ -40,7 +80,7 @@ const ViewArticle = ({ match }) => {
                 par {article.authorFirstname} {article.authorLastname}
             </p>
             <div>
-                Commentaires
+                 {renderedComments}
             </div>
         </Container>
     );
