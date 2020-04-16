91ef5b878b6b6cfad20a07ff5e137202536ad642
Sitael <beru666@live.fr>
2020-04-16 11:22:11 +0200

    add react-cookie

--- src/pages/Signin.js
+++ src/pages/Signin.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { useCookies } from 'react-cookie';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
@@ -8,6 +8,8 @@ import {toast} from "react-toastify";
 const Signin = () => {
     const [ email, setEmail ] = useState("");
     const [ password, setPassword ] = useState("");
+
+    const [ cookies, setCookie ] = useCookies();
 
     const handleChange = (event) => {
         const { name, value } = event.target;
@@ -40,8 +42,11 @@ const Signin = () => {
             .then((result) => {
                 return result.json();
             })
-            .then(({ status, extra }) => {
+            .then(({ status, extra, infos }) => {
                 if (status === "OK") {
+                    console.log(infos);
+                    setCookie("userToken", infos.userToken);
+                    setCookie("user", infos.user);
                     toast.success("tout semble s'être bien passé");
                 } else {
                     toast.error(
